063da4d9b0eeb7c0eba3deb6138b87217416e4f2
FloKots <florencekotohoyoh@gmail.com>
2022-06-02 15:23:46 +0300

    define location utility variables

--- main/models.py
+++ main/models.py
@@ -34,14 +34,39 @@ class Category(models.Model):
         self.last_updated = timezone.localtime(timezone.now())
         super(Category, self).save(*args, **kwargs)
 
+
 class Location(models.Model):
     name = models.CharField(null=True, blank=True, max_length=30)
 
+    #utility variable
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.uniqueId)
+    
+    def get_absolute_url(self):
+        return reverse('location-detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.name, self.uniqueId))
+
+        self.slug = slugify('{} {}'.format(self.name, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Location, self).save(*args, **kwargs)
+    
     def save_location(self):
         self.save()
     
     def delete_location(self):
         self.delete()
     
-    
+
     
