9ee4cbef6264ca5c2fe1815e8d51756db88750ee
FloKots <florencekotohoyoh@gmail.com>
2022-06-02 22:55:13 +0300

    define home page url

--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
--- personal_gallery/urls.py
+++ personal_gallery/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #Space Views
+    #Main App Views
     path('', include('main.urls')),
 ]
 
