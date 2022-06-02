4e669ef359000474c147762dc8919cbecc2de1b9
FloKots <florencekotohoyoh@gmail.com>
2022-06-03 02:43:27 +0300

    fetch categories

--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Location, Image
 
 # Create your views here.
 def home(request):
     title = 'Home'
-    return render(request, 'base-templates/index.html', {"title":title,} )
+    categories = Category.objects.all()
+    
+    return render(request, 'base-templates/index.html', {"title":title, "categories": categories,})
+
+
+def category_page(request):
+    return render(request, '', {})
+
+
+def location_page(request):
+    return render(request, '', {})
