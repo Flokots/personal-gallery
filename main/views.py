00683bcf8471f946254873dae0abcd4ba3c19118
FloKots <florencekotohoyoh@gmail.com>
2022-06-03 03:54:04 +0300

    add album css styles

--- main/views.py
+++ main/views.py
@@ -11,15 +11,16 @@ def home(request):
 
 
 def category_page(request, slug):
-
     category = Category.objects.get(slug=slug)
+    title=f'{Category.title}'
     images = Image.objects.filter(category=category)
    
-    return render(request, '', {"images": images, "category":category})
+    return render(request, '', {"images": images, "category":category, "title":title})
 
 
 def location_page(request, slug):
     location = Location.objects.get(slug=slug)
+    title = f'{Location.name}'
     images = Image.objects.filter(location=location)
 
-    return render(request, '', {"location": location, "images": images})
+    return render(request, '', {"location": location, "images": images, "title":title})
