665eab797f88b1f6c7d30c973d0698cefbaa5ae2
FloKots <florencekotohoyoh@gmail.com>
2022-06-03 03:29:57 +0300

    add favicons

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,13 @@
-from django.urls import re_path
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns=[
-    re_path(r'^$', views.home, name='home'),
-    re_path(r'^/category/<slug:slug>$', views.category_page, name='image-category'),
-    re_path(r'^/location/<slug:slug>$', views.location_page, name='image-location'),
+    path('', views.home, name='home'),
+    path('/category/<slug:slug>', views.category_page, name='image-category'),
+    path('/location/<slug:slug>', views.location_page, name='image-location'),
 ]
 
 if settings.DEBUG:
