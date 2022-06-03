735f54a9fc4b9464aa30c0cbf40069b59fb044d6
FloKots <florencekotohoyoh@gmail.com>
2022-06-03 06:10:42 +0300

    add image detail url patterns

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,8 @@ urlpatterns=[
     path('', views.home, name='home'),
     path('category/<slug:slug>', views.category_page, name='image-category'),
     path('location/<slug:slug>', views.location_page, name='image-location'),
+    path('category/<slug:slug>/<slug:slug>', views.category_image_detail, name='category-image-detail'),
+    path('location/<slug:slug>/<slug:slug>', views.location_image_detail, name='location-image-detail'),
 ]
 
 if settings.DEBUG:
