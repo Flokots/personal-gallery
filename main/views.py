adfccc36f644402a9581099164dd6ced434385d9
FloKots <florencekotohoyoh@gmail.com>
2022-06-03 03:10:33 +0300

    get category, location context variables

--- main/views.py
+++ main/views.py
@@ -10,9 +10,16 @@ def home(request):
     return render(request, 'base-templates/index.html', {"title":title, "categories": categories, "locations": locations})
 
 
-def category_page(request):
-    return render(request, '', {})
+def category_page(request, slug):
+
+    category = Category.objects.get(slug=slug)
+    images = Image.objects.filter(category=category)
+   
+    return render(request, '', {"images": images, "category":category})
 
 
-def location_page(request):
-    return render(request, '', {})
+def location_page(request, slug):
+    location = Location.objects.get(slug=slug)
+    images = Image.objects.filter(location=location)
+
+    return render(request, '', {"location": location, "images": images})
