9a72c77f1fd87a0aaca14f0c4e154915d575bc73
FloKots <florencekotohoyoh@gmail.com>
2022-06-03 11:18:30 +0300

    add test for location instance

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,52 @@
+from unicodedata import name
 from django.test import TestCase
+from .models import Category, Location, Image
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.gardens = Category(id=1, title='First Garden')
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.gardens, Category))
+
+    # Testing save method
+    def test_save_method(self):
+        self.gardens.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    
+    def test_delete_method(self):
+        self.gardens.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+    
+
+    def test_update_category(self):
+        self.gardens.title = 'New Garden'
+        self.gardens.save_category()
+        self.assertEqual(self.gardens.title, 'New Garden')
+
+    
+    class LocationTestClass(TestCase):
+        #Set up method
+        def setUp(self):
+            # Creating a new Location and saving it
+            self.new_location= Location(id=1, name='Nairobi')
+        
+        def tearDown(self):
+            Location.objects.all().delete()
+
+        
+        def test_instance(self):
+            self.assertTrue(isinstance(self.new_location, Location))
+
+        
+       
